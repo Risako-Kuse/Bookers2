505798fc646fb8362eef1e9b16913134532bc3b0
EC2 Default User <ec2-user@ip-172-31-6-204.ap-northeast-1.compute.internal>
2023-01-28 11:43:55 +0000

    20230128

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+   before_action :correct_user, only: [:edit, :update]
 # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
@@ -6,12 +7,19 @@ class BooksController < ApplicationController
     # UserIDを取り出す
     @book.user_id = current_user.id
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
+    if @book.save
     # 画面へリダイレクトし、フラッシュメッセージを表示させる
     flash[:notice] = "You have created book successfully."
     # 4. トップ画面へリダイレクト
-    redirect_to books_path(@book)
+    redirect_to book_path(@book)
+
+    else
+    @user = current_user
+    @books = Book.all
+    render :index
+    end
   end
+
 
   def index
      @book = Book.new
@@ -36,10 +44,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
 
@@ -47,6 +58,13 @@ class BooksController < ApplicationController
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :correct_user, only: [:edit, :update]
+
   def create
      # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
@@ -28,17 +30,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
     # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
-    flash[:notice] = "You have updated user successfully."
-    redirect_to user_path(user.id)
+     flash[:notice] = "You have updated user successfully."
+     redirect_to user_path(@user.id)
+    else
+     render :edit
+    end
   end
 
    private
 
   def user_params
-    params.require(:user).permit(:name, :image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user)) unless @user == current_user
   end
 
 end
